78c868beebbdfe0a5f33830e3659fb9212c02fac
Jorgediamanto <99408709+Jorgediamanto@users.noreply.github.com>
2022-04-24 18:10:28 +0200

    Add files via upload

---
+++ Ejercicios de Introducción a la Algorítmica. Entrega Individual 9.1.py
@@ -0,0 +1,8 @@
+numero1 = float(input("Introucir numero1= "))
+numero2 = float(input("Introucir numero2= "))
+numero3 = float(input("Introucir numero3= "))
+
+media = (numero1+numero2+numero3)/3
+
+
+print("La media aritmética de los tres numeros es = "+ str(media))
