92709b82d78f3c449c0a05df261352fb25287b06
Jorgediamanto <99408709+Jorgediamanto@users.noreply.github.com>
2022-04-24 20:55:37 +0200

    Delete Ejercicios de Introducción a la Algorítmica. Entrega Individual 10.1.py

--- Ejercicios de Introducción a la Algorítmica. Entrega Individual 10.1.py
+++
@@ -1,8 +0,0 @@
-lado = float(input("Introducir lado= "))
-altura_rel = float(input("Introducir altura relativa al lado= "))
-
-area = (lado*altura_rel)/2
-
-print("El area del triangulo es "+ str(area))
-print("Si el se introducen los lado perpendiculares de un triangulo rectangulo la ecuacion sigue funcionando")
-
