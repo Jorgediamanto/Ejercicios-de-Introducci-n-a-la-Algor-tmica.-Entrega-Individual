3cb65712648a99657a7f832acd394edf680c6a62
Jorgediamanto <99408709+Jorgediamanto@users.noreply.github.com>
2022-04-24 17:28:22 +0200

    Add files via upload

---
+++ Ejercicios de Introducción a la Algorítmica. Entrega Individual Ejercicio 8.1.py
@@ -0,0 +1,9 @@
+
+x=input("Introducir valor de precio sin impuestos: ")
+
+precio_sin_iva = x
+
+precio_con_impuestos= float(precio_sin_iva)*1.21
+
+print("Precio con impuestos: ")
+print(precio_con_impuestos)
